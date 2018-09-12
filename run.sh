6dc5e038a540a615a822decff1e22db3c3cfc5ef
ZzenlD <zzenld@posteo.de>
2018-09-12 15:02:08 +0200

    Variable updated

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-  nextcloudcmd $( [ "${NEXTCLOUD_TRUST_UNSECURE_CERT}" == true ] && echo "--trust" ) --non-interactive -u "${NEXTCLOUD_USER}" -p "${NEXTCLOUD_PASSWORD}" /nextcloud "${NEXTCLOUD_URL}"
+  nextcloudcmd $( [ "${NEXTCLOUD_TRUST_UNSECURE_CERT}" == true ] && echo "--trust" ) --non-interactive -u "${NEXTCLOUD_USERNAME}" -p "${NEXTCLOUD_PASSWORD}" /nextcloud "${NEXTCLOUD_URL}"
   sleep "${NEXTCLOUD_SYNC_INTERVAL}"
 done
