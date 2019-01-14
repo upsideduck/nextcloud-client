766c6fcadefb528c793e2d6a8f0cea9310779f22
Johan Adell <johan@upsideduck.com>
2019-01-14 13:19:51 +0100

    Changed to user exclude list and run as specified user ogfgroup

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,23 @@
 FROM alpine:latest
-MAINTAINER ZzenlD
+MAINTAINER Upsideduck
+
+ARG USER=ncsync
+ARG PUID=1000
+ARG PGID=1000
+
+ENV NEXTCLOUD_SYNC_INTERVAL=900
+ENV USER=$USER \
+    PUID=$PUID \
+    PGID=$PGID 
+
+# create group and user
+RUN addgroup -g $PGID $USER && adduser -G $USER -D -u $PUID $USER
+
 
 RUN apk add --no-cache nextcloud-client && \
     mkdir nextcloud
 
 COPY run.sh /run.sh
-
-ENV NEXTCLOUD_SYNC_INTERVAL=900
+COPY exclude.lst /exclude.lst
 
 CMD /run.sh
--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
-#!/bin/sh
+#!/bin/ash
 
-while true; do
-  nextcloudcmd $( [ "${NEXTCLOUD_TRUST_UNSECURE_CERT}" == true ] && echo "--trust" ) --non-interactive -u "${NEXTCLOUD_USERNAME}" -p "${NEXTCLOUD_PASSWORD}" /nextcloud "${NEXTCLOUD_URL}"
+# ensure, that the sync dir exists and is owned by the user
+#[ -d /nextcloud ] || mkdir -p /nextcloud
+echo "chown $USER:$USER /nextcloud"
+chown -R $USER:$USER /nextcloud
+
+for i in $TEMP
+do
+    echo $i >> /exclude.lst
+done
+
+while true; 
+do
+  /bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ "${NEXTCLOUD_TRUST_UNSECURE_CERT}" == true ] && echo "--trust" ) --non-interactive --unsyncedfolders /exclude.lst -u "${NEXTCLOUD_USERNAME}" -p "${NEXTCLOUD_PASSWORD}" /nextcloud "${NEXTCLOUD_URL}"'
   sleep "${NEXTCLOUD_SYNC_INTERVAL}"
 done
